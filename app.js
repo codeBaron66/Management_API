549563188c75404e68e1b690e2681f48a2a850df
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-16 17:31:02 +0000

    multipart JS added

---
+++ app.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('#form');
+const fileName = document.querySelector('#filename');
+const fileInput = document.getElementById('chooseFile');
+
+form.addEventListener('submit', createMedia);
+
+const resp = [];
+
+function uploadParts(){
+    let URL = `https://api.jwplayer.com/v2/uploads/${resp[0].upload_id}/parts?page=1&page_length=50`;
+    fetch(URL, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': resp[0].upload_token
+        }
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+}
+
+function createMedia(e){
+    e.preventDefault();
+    let URL = 'https://api.jwplayer.com/v2/sites/CivsmZGh/media/';
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'gG_69lS7o52T-G8dc-Y4tmInVlVKbWQybHBNbEZUZG5OdFJ6aExjbnB6Vm5kak0xbHIn'
+        },
+        body: JSON.stringify({
+            upload: {
+                'method': 'multipart',
+                'mime_type': 'video/mp4',
+            },
+            metadata: {
+                'title': fileName.value,
+                'description': 'uploading using APIv2',
+                'category': 'Careers',
+                'language': 'en'
+            }
+        })
+    })
+    .then(response => response.json())
+    .then(function(res){
+        let id = res["upload_id"];
+        let token = res["upload_token"]
+        resp.push({
+            "upload_id": id,
+            "upload_token": token
+        });
+    });
+    setTimeout(function(){
+        uploadParts();
+    }, 1000);
+}
