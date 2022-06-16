c0e0567bb3af11989e35cfcc6cce8057705c0e12
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-16 21:59:01 +0000

    Most of JS complete

--- app.js
+++ app.js
@@ -3,16 +3,13 @@ const fileName = document.querySelector('#filename');
 const fileInput = document.getElementById('chooseFile');
 
 form.addEventListener('submit', createMedia);
-
 const resp = [];
 
-function uploadParts(){
-    let URL = `https://api.jwplayer.com/v2/uploads/${resp[0].upload_id}/parts?page=1&page_length=50`;
-    fetch(URL, {
-        method: "GET",
+function uploadMedia(){
+    fetch(resp[0], {
+        method: "PUT",
         headers: {
-            'Content-Type': 'application/json',
-            'Authorization': resp[0].upload_token
+            'Content-Type': 'video/mp4',
         }
     })
     .then(response => response.json())
@@ -31,7 +28,7 @@ function createMedia(e){
         },
         body: JSON.stringify({
             upload: {
-                'method': 'multipart',
+                'method': 'direct',
                 'mime_type': 'video/mp4',
             },
             metadata: {
@@ -44,14 +41,8 @@ function createMedia(e){
     })
     .then(response => response.json())
     .then(function(res){
-        let id = res["upload_id"];
-        let token = res["upload_token"]
-        resp.push({
-            "upload_id": id,
-            "upload_token": token
-        });
+        let upLink = res["upload_link"];
+        resp.push(upLink)
+        uploadMedia();
     });
-    setTimeout(function(){
-        uploadParts();
-    }, 1000);
 }
