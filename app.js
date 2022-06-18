ae20eacead526350df0d99c37f52cb73771ea13e
codeBaron66 <40766571+codeBaron66@users.noreply.github.com>
2022-06-18 22:10:12 +0000

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,54 @@ function createMedia(e){
         uploadParts();
     }, 1000);
 }
+
+
+// DIRECT UPLOAD
+const form = document.querySelector('#form');
+const fileName = document.querySelector('#filename');
+const fileInput = document.getElementById('chooseFile');
+
+form.addEventListener('submit', createMedia);
+const resp = [];
+
+function uploadMedia(){
+    fetch(resp[0], {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'video/mp4',
+        }
+    })
+    .then(response => response.json())
+    .then(response => console.log(response))
+}
+
+function createMedia(e){
+    e.preventDefault();
+    let URL = 'https://api.jwplayer.com/v2/sites/CivsmZGh/media/';
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'gG_69lS7o52T-G8dc-Y4tmInVlVKbWQybHBNbEZUZG5OdFJ6aExjbnB6Vm5kak0xbHIn'
+        },
+        body: JSON.stringify({
+            upload: {
+                'method': 'direct',
+                'mime_type': 'video/mp4',
+            },
+            metadata: {
+                'title': fileName.value,
+                'description': 'uploading using APIv2',
+                'category': 'Careers',
+                'language': 'en'
+            }
+        })
+    })
+    .then(response => response.json())
+    .then(function(res){
+        let upLink = res["upload_link"];
+        resp.push(upLink)
+        uploadMedia();
+    });
+}
